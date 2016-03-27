2d19fa6797ee5078c664b913330c14a832cb4cc7
Roberto Carrillo <email@robertocarrillo.com>
2016-03-26 23:58:30 -0500

    Updated components

--- UsersGroups.Web/wwwroot/_references.js
+++ UsersGroups.Web/wwwroot/_references.js
@@ -4,5 +4,3 @@
 /// <reference path="lib/angular/angular.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
-/// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
-/// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
