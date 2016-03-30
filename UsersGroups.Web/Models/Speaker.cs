bd01238354cbea93835dcf805842282e490b2f3b
Roberto Carrillo <email@robertocarrillo.com>
2016-03-30 05:18:29 -0500

    Forgot file

---
+++ UsersGroups.Web/Models/Speaker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace UsersGroups.Web.Models
+{
+    public class Speaker
+    {
+        public int SpeakerId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public ICollection<Meeting> Meetings { get; set; } = new HashSet<Meeting>();
+    }
+}
